275a37ba385566d7855d3bff2ad35993ae12514d
tofuqwq1145 <tofu.qwq1145@gmail.com>
2026-08-11 20:59:45 +0800

    text: 更新关于本站文本

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,12 +6,12 @@ export default function AboutPage() {
       <Breadcrumb items={[{ label: '关于 KRTPedia' }]} />
       <h1 className="font-serif text-4xl mb-12 text-archive-text border-b border-archive-border pb-6">关于 KRTPedia</h1>
       <div className="bg-archive-paper border border-archive-border p-8 md:p-12 text-archive-text leading-loose">
-        <h2 className="font-serif text-2xl mb-8 border-l-4 border-archive-accent pl-4">发起宣言</h2>
+        <h2 className="font-serif text-2xl mb-8 border-l-4 border-archive-accent pl-4">关于本站</h2>
         <div className="space-y-6 text-sm">
-          <p>在过去的岁月中，无数伟大的建筑拔地而起，无数惨烈的战争改变了版图，无数杰出的领袖与成员留下了他们的印记。</p>
-          <p>然而现在，时间的流逝，这些辉煌正在逐渐被遗忘。我们曾经历过没有任何历史记录的时代，那导致我们除了依靠越来越模糊的记忆和少数的截图外，别无他法去证明我们曾经创造过什么。</p>
-          <p>这是文明的断层，也是我们建立 KRTP 的初衷。</p>
-          <p className="font-bold text-archive-accent text-lg py-4">所以我在此宣布：KRTP项目，正式开启！</p>
+          <p>KRTP（KRTpedia） 是 KRT 中世纪国战服务器的历史与设定知识库。它由“Kachibode”发起，并面向全服务器所有国家和玩家开放共建。</p>
+          <p>不同于以往散落在 QQ 群聊、零星截图或玩家个人记忆中的碎片化信息，KRTP 致力于打造一个客观、严谨、系统化的维基式百科。</p>
+          <p>在这里，我们将收录：国家、编年史、人物、建筑、事件等一系列相关内容</p>
+          <p className="font-bold text-archive-accent text-lg py-4">KRTP 并非属于某一个国家的私有档案，而是一项服务器的公益项目。</p>
         </div>
       </div>
       <div className="mt-8 text-center">
